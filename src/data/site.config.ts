a3e0208bd510dc79c47ec0e3ff7ffcd4769a3776
ets-certs <149312629+ets-certs@users.noreply.github.com>
2024-10-28 23:49:50 +0800

    Update site.config.ts

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -9,11 +9,11 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'DanielCG', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
+	author: 'Never', // Site author
+	title: '远程识别 Remote-ID', // Site title.
 	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
-	lang: 'en-GB',
-	ogLocale: 'en_GB',
+	lang: 'zh-CN',
+	ogLocale: 'zh_CN',
 	shareMessage: 'Share this post', // Message to share a post on social media
 	paginationSize: 6 // Number of posts per page
 }
