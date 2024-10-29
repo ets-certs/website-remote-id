e0959c6662b371a657a849f53d550e671e536c54
NeverMin <never.min@gmail.com>
2024-10-29 16:29:08 +0800

    added contact page

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,2 +1,2 @@
 // List of categories for blog posts
-export const CATEGORIES = ['美国 FAA', '欧盟', '中国 GB47590', '日本', '中东'] as const
+export const CATEGORIES = ['美国 FAA', '欧盟', '中国 GB47590', '日本', '中东', '宜安特'] as const
